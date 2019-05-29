769dc89d53b21055f5a4e9b9779ea42765c9cfef
s414june <48972804+s414june@users.noreply.github.com>
2019-05-29 11:21:21 +0800

    Add files via upload

---
+++ wk15/wk15.ino
@@ -0,0 +1,97 @@
+const int buttonPin = 2;     // the number of the pushbutton pin
+int latchPin = 8;
+int clockPin = 12;
+int dataPin = 11;
+byte colDataMatrix[8] = { B01111111,
+   B10111111,
+   B11011111,
+   B11101111,
+   B11110111,
+   B11111011,
+   B11111101,
+   B11111110
+  };
+                      
+byte rowDataMatrix[8] = {   B11111111,
+    B10000001,
+    B10000001,
+    B10000001,
+    B10000001,
+    B10000001,
+    B10000001,
+    B11111111
+  };
+
+byte rowDatabutton[8] = { 
+B00000000,
+B00000000,
+B00100100,
+B00000000,
+B01000010,
+B00111100,
+B00000000,
+B00000000
+  };
+ 
+int buttonState = 0;      // buttont starting state
+// 定義顯示器顯示的時間 (延遲時間), 預設 1ms
+int delay_time = 1;
+       
+void setup() {
+pinMode(buttonPin, INPUT);
+pinMode(latchPin,OUTPUT);
+pinMode(clockPin,OUTPUT);
+pinMode(dataPin,OUTPUT);
+}
+void loop() {
+ buttonState = digitalRead(buttonPin);   //read the button
+   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+     for(int i = 0; i < 8 ; i++){
+ //byte colData = colDataMatrix[i];
+digitalWrite(latchPin,LOW);
+  //  pickDigit(i);
+   shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+    shiftOut(dataPin, clockPin, MSBFIRST, rowDataMatrix[i]);   //
+    //shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+    digitalWrite(latchPin,HIGH);
+    delay(delay_time);
+  }
+} 
+  else {
+    // turn LED off:
+     for(int i = 0; i < 8 ; i++){
+ //byte colData = colDataMatrix[i];
+digitalWrite(latchPin,LOW);
+  //  pickDigit(i);
+   shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+    shiftOut(dataPin, clockPin, MSBFIRST, rowDatabutton[i]);   //
+    //shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+    digitalWrite(latchPin,HIGH);
+    delay(delay_time);
+  }
+}
+  
+/* for(int i = 0; i < 8 ; i++){
+ //byte colData = colDataMatrix[i];
+digitalWrite(latchPin,LOW);
+  //  pickDigit(i);
+   shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+    shiftOut(dataPin, clockPin, MSBFIRST, rowDataMatrix[i]);   //
+    //shiftOut(dataPin, clockPin, MSBFIRST, colDataMatrix[i]);
+    digitalWrite(latchPin,HIGH);
+    delay(delay_time);
+  }  */
+  
+}
+
+
+
+
+void pickDigit(int x) {
+  byte rowByte = 1 << x;
+  shiftOut(dataPin, clockPin, MSBFIRST, rowByte);
+
+}
+
